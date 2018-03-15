25cd45a1d23ed65bd7e99768db81a2d6152b8695
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-15 18:52:32 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    session[:user_id] = @user.id
-    redirect_to users_path
+    if @user 
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else 
+      redirect_to new_user_path
+    end
   end
 
   private
