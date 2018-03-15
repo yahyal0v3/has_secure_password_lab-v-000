be8a92dc940a23b6a5f4a2afcc98868db42f768a
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-15 18:27:57 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to users_path
     else
-      return head(:forbidden)
+      redirect_to new_user_path
     end
   end
 end
