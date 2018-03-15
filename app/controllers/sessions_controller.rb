42b2579f5a5df1e19ad21659c2d19aaa1be87370
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-15 18:59:39 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      redirect_to new_user_path
+    end
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.errors.empty?
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
